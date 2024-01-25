4101e94f2087d086e1db670f3710270a0fe0bbd5
ForageIt <katariaesha02@gmail.com>
2024-01-25 11:03:21 +0530

    25-01-2024

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ mongoDB();
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin","https://restro-wbno.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin","https://restro-wbno.vercel.app/");
   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header(
